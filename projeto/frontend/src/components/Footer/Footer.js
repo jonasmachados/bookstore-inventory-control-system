4d5d2d2c4298b43041f4a64a20fad7475250ece3
jonasmachados <jonasmachado.ti@gmail.com>
2022-11-14 16:17:25 -0300

    Fix Icon Social Media Footer

--- projeto/frontend/src/components/Footer/Footer.js
+++ projeto/frontend/src/components/Footer/Footer.js
@@ -1,10 +1,10 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import logo from "../../assets/img/logo.png";
-import navIcon1 from "../../assets/img/nav-icon1.png";
-import navIcon2 from "../../assets/img/nav-icon2.png";
-import navIcon3 from "../../assets/img/nav-icon3.png";
 import "./Footer.css";
+
+import {FaLinkedinIn, FaFacebookF } from 'react-icons/fa';
+import {AiFillInstagram } from 'react-icons/ai';
 
 const Footer = () => {
   return (
@@ -19,14 +19,14 @@ const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#">
-                <img src={navIcon1} alt="Icon" />
+              <a href="https://www.facebook.com/jonas.machado.90/">
+                <i><FaFacebookF /></i>
               </a>
-              <a href="#">
-                <img src={navIcon2} alt="Icon" />
+              <a href="https://www.instagram.com/jonasmachado01/">
+                <i> <AiFillInstagram /></i>
               </a>
-              <a href="#">
-                <img src={navIcon3} alt="Icon" />
+              <a href="https://www.linkedin.com/in/jonas-machados/">
+                <i><FaLinkedinIn /></i>
               </a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
