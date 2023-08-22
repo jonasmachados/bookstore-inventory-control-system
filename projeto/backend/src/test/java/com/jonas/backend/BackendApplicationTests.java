5156765bdd34e9d7e9336cb64ff40c4d1f2a2194
jonasmachados <jonasmachado.ti@gmail.com>
2023-08-22 10:04:49 -0300

    :wrench: chore(back): Integrate Testing Framework and Plugins

--- projeto/backend/src/test/java/com/jonas/backend/BackendApplicationTests.java
+++ projeto/backend/src/test/java/com/jonas/backend/BackendApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void main() {
+        BackendApplication.main(new String[]{});
+    }
 
 }
