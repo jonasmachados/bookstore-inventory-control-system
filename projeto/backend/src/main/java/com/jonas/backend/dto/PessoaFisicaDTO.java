1883e36cc8129b845074fa9c90244a5f5cbeb96d
jonasmachados <jonasmachado.ti@gmail.com>
2023-08-27 12:42:53 -0300

    :recycle: refactor(dto): Add overloaded constructors to PessoaFisicaDTO and PessoaJuridicaDTO

--- projeto/backend/src/main/java/com/jonas/backend/dto/PessoaFisicaDTO.java
+++ projeto/backend/src/main/java/com/jonas/backend/dto/PessoaFisicaDTO.java
@@ -1,17 +1,26 @@
 package com.jonas.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.List;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class PessoaFisicaDTO extends ClienteDTO {
-    
+
     private String cpf;
     private String rg;
 
+    public PessoaFisicaDTO(Long id, String nome, String rua, Integer numero,
+            String bairro, String cidade, String estado, String cep,
+            String rg, String cpf, List<VendaDTO> vendas) {
+        super(id, nome, rua, numero, bairro, cidade, estado, cep, vendas);
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
 }
-
-
--- projeto/backend/src/main/java/com/jonas/backend/dto/PessoaJuridicaDTO.java
+++ projeto/backend/src/main/java/com/jonas/backend/dto/PessoaJuridicaDTO.java
@@ -1,15 +1,24 @@
 package com.jonas.backend.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.List;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class PessoaJuridicaDTO extends ClienteDTO {
 
     private String cnpj;
 
+    public PessoaJuridicaDTO(Long id, String nome, String rua, Integer numero,
+            String bairro, String cidade, String estado, String cep,
+            String cnpj, List<VendaDTO> vendas) {
+        super(id, nome, rua, numero, bairro, cidade, estado, cep, vendas);
+        this.cnpj = cnpj;
+    }
 
 }
