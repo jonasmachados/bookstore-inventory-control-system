14c1b36046468189c69d50ceff26423d918e2ade
Jonas Machado <jonasmachado.ti@gmail.com>
2023-10-10 15:23:05 -0300

    :sparkles: Frontend - feat: Create RecentPurchase component for recent sales

---
+++ projeto/frontend/src/components/RecentPurchases.js
@@ -0,0 +1,42 @@
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import "../styles/table.css"
+import { convertNumberToCurrency } from "../utils/convertNumberToCurrency";
+import TitleWithIcon from './TitleWithIcon';
+
+const RecentPurchases = ({ purchaseList }) => {
+
+    return (
+        <>
+
+            <TitleWithIcon
+                icon={<AiOutlineShoppingCart />}
+                title="Compras Recentes"
+            />
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th> <p>Livro </p> </th>
+                        <th> <p>Quantidade</p> </th>
+                        <th> <p>Preço </p> </th>
+                        <th> <p>Total </p> </th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {purchaseList.slice(-4).map((compra) => (
+                        <tr key={compra.id}>
+                            <td> <p>{compra.livro.titulo} </p> </td>
+                            <td> <p>{compra.qtdItens} </p> </td>
+                            <td> <p>{convertNumberToCurrency(compra.precoVenda)} </p> </td>
+                            <td> <p>{convertNumberToCurrency(compra.total)} </p> </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+        </>
+    );
+};
+
+export default RecentPurchases;
