d071a22270c7812507e8e7d81ca35f5fc44c22fb
Jonas Machado <jonasmachado.ti@gmail.com>
2023-10-17 18:08:15 -0300

    :sparkles: Frontend - feat: Created a BookList component to display a list of books.

---
+++ projeto/frontend/src/components/BookList.js
@@ -0,0 +1,90 @@
+import { Link } from "react-router-dom";
+import livroImg from "../assets/img/livro.svg";
+import livroToPDF from "../report/cliente/LivroToPDF";
+import "../styles/table.css"
+import "../styles/button.css"
+import { AiOutlinePrinter, AiOutlineFolderAdd, AiOutlineDelete } from 'react-icons/ai'
+import { BiEdit } from 'react-icons/bi'
+import { GiBookmarklet } from 'react-icons/gi'
+import TitleWithIcon from "./TitleWithIcon";
+import formatDateToDdMonthYyyy from "../utils/formatDateToDdMonthYyyy";
+
+const BookList = ({ bookList, onDeleteBook }) => {
+
+  return (
+    <>
+
+      <div className="table-title">
+        <TitleWithIcon
+          icon={<GiBookmarklet />}
+          title="Livros"
+        />
+      </div>
+
+      <div className="button-container">
+        <Link to="/add-livro" className="button primary-button">
+          <AiOutlineFolderAdd className="button-icon" />
+          Novo
+        </Link>
+
+        <button
+          onClick={(e) => livroToPDF(bookList)}
+          className="button secondary-button" >
+          <AiOutlinePrinter className="button-icon" />
+          Print
+        </button>
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th> <p>Id</p> </th>
+            <th> <p>Titulo</p> </th>
+            <th> <p>Autor</p> </th>
+            <th>Editora</th>
+            <th>Capa</th>
+            <th>Publicação</th>
+            <th> <p>Estoque</p> </th>
+            <th> <p>Ações</p> </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {bookList.map((book) => (
+            <tr key={book.id}>
+              <td> <p>{book.id}</p></td>
+              <td> <p>{book.titulo}</p> </td>
+              <td> <p>{book.autor}</p> </td>
+              <td> <p>{book.editora} </p> </td>
+              <td> <p><img className="book-cover-image" src={book.linkImg} alt="CapaLivro" /> </p> </td>
+              <td> <p>{formatDateToDdMonthYyyy(book.anoPublicacao)} </p> </td>
+              <td> <p>{book.estoque} </p> </td>
+              <td>
+                <div className="table-action">
+                  <Link
+                    className="table-action-icon edit"
+                    to={`/edit-livro/${book.id}`} >
+                    <BiEdit />
+                  </Link>
+
+                  <button
+                    className="table-action-icon"
+                    onClick={() => onDeleteBook(book.id)}>
+                    <AiOutlineDelete />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div class="table-img">
+        <img
+          src={livroImg} alt="Book Img" />
+      </div>
+    </>
+  );
+};
+
+export default BookList;
