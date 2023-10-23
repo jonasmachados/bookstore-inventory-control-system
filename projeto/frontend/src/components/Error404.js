ce345a2bd4f1b3cad18f4f2d727fdc87df5b5815
Jonas Machado <jonasmachado.ti@gmail.com>
2023-10-23 18:20:13 -0300

    :sparkles: Frontend - feat: Develop Error404 component with HTML content

---
+++ projeto/frontend/src/components/Error404.js
@@ -0,0 +1,15 @@
+import "../styles/error404.css"
+
+const Error404 = () => {
+    return (
+        <div className="error404-container">
+
+            <h1>Error</h1>
+
+            <h2>Oops! Parece que você acessou uma página que não existe!</h2>
+
+        </div>
+    );
+};
+
+export default Error404;
