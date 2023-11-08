ccd9a2b01ff3751ce44f4b3c52871fd86bfc2699
Jonas Machado <jonasmachado.ti@gmail.com>
2023-11-08 09:20:14 -0300

    :sparkles: Frontend - feat: Create SaleListPage.

---
+++ projeto/frontend/src/pages/SaleFormPage.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import LivroService from '../services/LivroService';
+import SaleService from '../services/SaleService';
+import SaleForm from '../components/SaleForm';
+import CustomerService from '../services/CustomerService';
+
+const SaleFormPage = () => {
+
+    const { id } = useParams();
+    const [sale, setSale] = useState([]);
+    //  const [saleList, setSaleList] = useState([]);
+    const [bookList, setBookList] = useState([]);
+    const [customerList, setCustomerList] = useState([]);
+
+    useEffect(() => {
+        const fetchCustomers = async () => {
+            try {
+                const response = await CustomerService.findAllCustomers();
+                setCustomerList(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        const fetchBooks = async () => {
+            try {
+                const response = await LivroService.getAllLivros();
+                setBookList(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        const fetchSale = async () => {
+            try {
+                const response = await SaleService.findSaleById(id);
+                setSale(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        if (id) {
+            fetchSale();
+        }
+        fetchBooks();
+        fetchCustomers();
+    }, [id]);
+
+    return (
+
+        <SaleForm
+            id={id}
+            sale={sale}
+            bookList={bookList}
+            customerList={customerList}
+        />
+
+    );
+
+};
+
+export default SaleFormPage;
