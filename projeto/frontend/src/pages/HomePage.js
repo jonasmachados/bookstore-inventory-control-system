94817d490718098d06ffd11dddd879cb52d77dd2
Jonas Machado <jonasmachado.ti@gmail.com>
2023-11-10 16:52:46 -0300

    :recycle: Frontend - refactor: Refactor method name and BookService in HomePage
    - Renamed the useEffect handling method for improved clarity.
    - Updated references to 'bookService' for consistency.

--- projeto/frontend/src/pages/HomePage.js
+++ projeto/frontend/src/pages/HomePage.js
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 import Dashboard from "../components/Dashboard";
 import RecentPurchases from "../components/RecentPurchases";
 import RecentSales from "../components/RecentSales";
-import LivroService from "../services/LivroService"
-import ClienteService from "../services/ClienteService"
-import CompraService from "../services/CompraService";
-import VendaService from "../services/VendaService";
-
-import "../styles/home-page.css"
+import BookService from "../services/BookService";
+import CustomerService from "../services/CustomerService";
+import SaleService from "../services/SaleService";
+import PurchaseService from "../services/PurchaseService";
+import "../styles/page.css";
 
 const HomePage = () => {
 
@@ -17,58 +16,50 @@ const HomePage = () => {
     const [saleList, setSaleList] = useState([]);
 
     useEffect(() => {
-        getAllLivros();
-        getAllClientes();
-        getAllCompras();
-        getAllVendas();
+        fetchBooks();
+        fetchCustomers();
+        fetchPurchases();
+        fetchSales();
     }, []);
 
-    const getAllLivros = () => {
-        LivroService.getAllLivros()
-            .then((response) => {
-                setBookList(response.data);
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const fetchBooks = async () => {
+        try {
+            const response = await BookService.findAllBooks();
+            setBookList(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    const getAllClientes = () => {
-        ClienteService.getAllClientes()
-            .then((response) => {
-                setCustomerList(response.data);
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const fetchCustomers = async () => {
+        try {
+            const response = await CustomerService.findAllCustomers();
+            setCustomerList(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    const getAllCompras = () => {
-        CompraService.getAllCompras()
-            .then((response) => {
-                setPurchaseList(response.data);
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const fetchPurchases = async () => {
+        try {
+            const response = await PurchaseService.findAllPurchase();
+            setPurchaseList(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
-    const getAllVendas = () => {
-        VendaService.getAllVendas()
-            .then((response) => {
-                setSaleList(response.data);
-                console.log(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const fetchSales = async () => {
+        try {
+            const response = await SaleService.findAllSales();
+            setSaleList(response.data);
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
-        <div className="home-page-container">
+        <div className="container-page">
 
             <Dashboard
                 customerList={customerList}
